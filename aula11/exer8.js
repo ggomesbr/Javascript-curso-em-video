2c0c47403df5bc421fa855cfaf705759cafa9d71
Gabriel Luiz <gabriel.luiz77888@gmail.com>
2024-10-01 13:04:43 -0300

    Criando página para o exercício 18

--- aula11/exer8.js
+++
@@ -1,7 +0,0 @@
-pais = 'Brasil';
-console.log(`Vivendo no ${pais}`);
-if (pais == 'Brasil') {
-    console.log('Você é brasileiro!');
-} else {
-    console.log('Você é estrangeiro!');
-}
--- aula11/exer9.js
+++
@@ -1,7 +0,0 @@
-var vel = 70
-
-if (vel > 60) {
-    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
-}else {
-    console.log('Dirija sempre com segurança!')
-}
